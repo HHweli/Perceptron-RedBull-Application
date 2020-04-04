7f02af939b195d731e6e4604e3ab9bfb761a54f9
umadhg1 <geethsandaru.madhushan@pearson.com>
2020-04-04 19:43:25 +0530

    prediction result page ui

---
+++ Perceptron RedBull Application/PredictionResultPage.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Perceptron_RedBull_Application
+{
+    partial class PredictionResultPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PredictionResultPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Perceptron_RedBull_Application.Properties.Resources.Result_display_page_background_image;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.MaximumSize = new System.Drawing.Size(720, 480);
+            this.MinimumSize = new System.Drawing.Size(720, 480);
+            this.Name = "PredictionResultPage";
+            this.Text = "Prediction Result";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
