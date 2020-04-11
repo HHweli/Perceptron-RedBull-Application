264f1ad24ede9cd0fb8e934bd5fe96e66e4e8c6a
umadhg1 <geethsandaru.madhushan@pearson.com>
2020-04-11 11:30:13 +0530

    predictor

--- Perceptron RedBull Application/ML/Service/Predictor.cs
+++ Perceptron RedBull Application/ML/Service/Predictor.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using static Microsoft.ML.DataOperationsCatalog;
 
 namespace Perceptron_RedBull_Application.ML.Service
 {
